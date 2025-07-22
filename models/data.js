9c84383492b41ade299045df3e7cbc8f173fa0f7
gauravbisen1 <gouravbisen123@gmail.com>
2025-07-23 01:25:58 +0530

    ui implementing..

--- models/data.js
+++ models/data.js
@@ -2,9 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const dataSchema = new Schema({
+    user: { type: String, required: true },
     text: { type: String, required: true },
     sentiment: { type: String },
-    date: { type: Date, default: Date.now }
+    date: { 
+        type: String, 
+        default: () => {
+            const now = new Date();
+            const day = String(now.getDate()).padStart(2, '0');
+            const month = now.toLocaleString('default', { month: 'long' });
+            const year = now.getFullYear();
+            return `${day} ${month} ${year}`;
+        }
+    },
 });
 
 const data = mongoose.model("data",dataSchema);
